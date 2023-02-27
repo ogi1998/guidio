d07a7782cb2030dc51adc930549b317bacbca1fc
ogi1998 <38105408+ogi1998@users.noreply.github.com>
2023-02-27 19:53:51 +0100

    Change courses grid
    - Changed courses container to use `grid` instead of `flexbox`
    - Changed number of elements in a row from `3` to `4`

--- src/components/pages/Landing/Courses.js
+++ src/components/pages/Landing/Courses.js
@@ -20,9 +20,9 @@ const Courses = () => {
 	return (
 		<div>
 			<Dropdown title="Popular" items={dropdownItems} />
-			<div className="flex flex-wrap gap-20 w-full">
+			<div className=" grid grid-cols-4 w-full gap-5">
 				{[1, 2, 3, 4, 5].map(item =>
-				<NavLink to='/' className="group w-[30%] mb-10 bg-light-main hover:cursor-pointer" key={item}>
+				<NavLink to='/' className="group w-full mb-10 bg-light-main hover:cursor-pointer" key={item}>
 					<div className="relative">
 						<img src={cardImg} alt="Card Item" />
 						<div className="
@@ -30,13 +30,13 @@ const Courses = () => {
 						flex items-end gap-2
 						invisible group-hover:visible
 						bg-gradient-to-b from-gradient-white to-gradient-secondary">
-							<h3 className="text-4xl">Clara Jackson |</h3>
+							<h3 className="text-2xl">Clara Jackson |</h3>
 							<span className="text-lg">programmer</span>
 						</div>
 					</div>
 					<div className="flex items-center gap-2 px-2 py-4 shadow-secondary-main shadow-normal rounded-b-3xl">
 						<FaUser className="rounded-[50%] bg-success-main text-2xl p-1" />
-						<p className="text-2xl">Lorem ipsum dolor sit amet co.</p>
+						<p className="text-lg">Lorem ipsum dolor sit amet</p>
 						<span className="flex items-center gap-1 text-xl"><FaHeart /> 132</span>
 						<span className="flex items-center gap-1 text-xl"><FaEye /> 39k</span>
 					</div>
