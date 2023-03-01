c89405ffe0f8b7c452754e716f090d66ace73489
ogi1998 <ogivuja@gmail.com>
2023-03-01 10:22:53 +0100

    Minor - Logo
    - Changed Logo
    - Added `favicon` icon

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,15 +1,16 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 const Logo = ({ inFooter = false }) => {
 
 
 	return (
-		<div className={`
+		<NavLink  to='/' className={`
 		${!inFooter && "absolute left-1/2 -translate-y-8 -translate-x-1/2 bg-primary-main py-8 px-8 rounded-[50%]"}
 		${inFooter && "mb-5"}
 		`}>
-			<img src={logo} alt="Guid.io Logo" />
-		</div>
+			<img src={logo} alt="Guid.io Logo" className=' w-36'/>
+		</NavLink>
 	);
 };
 export default Logo;
--- src/components/ui/Form.js
+++ src/components/ui/Form.js
@@ -7,8 +7,8 @@ const Form = (props) => {
 		<div className=" w-screen h-screen bg-form bg-cover flex justify-center items-center">
 		<div className="w-1/2 bg-form bg-cover flex shadow-normal-focused shadow-secondary-main">
 			<div className="w-1/2 relative">
-				<NavLink to='/' className="bg-secondary-main p-10 rounded-[50%] absolute left-1/4 top-10">
-					<img src={logo} alt="Guidio logo" />
+				<NavLink to='/' className="bg-primary-main p-10 rounded-[50%] absolute left-1/4 top-10">
+					<img src={logo} alt="Guidio logo" className=' w-36' />
 				</NavLink>
 			</div>
 			<form className="py-10 mr-10 w-1/2">
