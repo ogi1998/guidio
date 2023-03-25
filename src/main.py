5e5e8428bdace862120fca0ffd8ee3cbb5598be6
Amar Badnjevic <badnja25@gmail.com>
2023-03-25 20:11:21 +0100

    fix: media files path and app media mount

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from auth import router as auth_router
+from core.constants import MEDIA_ROOT
 from guides import router as guides_router
 from users import router as users_router
 
 app = FastAPI()
 
+
+app.mount("/media", StaticFiles(directory=MEDIA_ROOT), name="media")
 app.include_router(auth_router.router,
                    prefix="/auth",
                    tags=["auth"])
