3df7bf5812347d7e3bdf09094be77700d1346f5d
Amar Badnjevic <badnja25@gmail.com>
2023-06-26 15:06:19 +0200

    feat: hide docs from prod environment
    Added config file for general configs, including env
    Moved database config
    Modified startup of an app to check for environment

--- src/database.py
+++ src/database.py
@@ -1,16 +1,8 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
-load_dotenv()
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
